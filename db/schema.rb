912e4682afc5553f04f7c983d9d3809699c23c10
valenlyn <valenlynchua@gmail.com>
2019-05-05 19:19:42 +0800

    Fixed tiny bug in style.scss; changed default GR images to Readdit's in db/seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,18 @@ ActiveRecord::Schema.define(version: 2019_05_05_063233) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rating_caches", force: :cascade do |t|
+    t.string "cacheable_type"
+    t.bigint "cacheable_id"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable_type_and_cacheable_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "book_id"
