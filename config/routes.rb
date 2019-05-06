0347e299bc8132d8ed27dd746ec756bf49902918
valenlyn <valenlynchua@gmail.com>
2019-05-06 17:47:01 +0800

    Added bookstatus MVC. Rendered partial form for new bookstatus in Reviews index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books, :booklists, :reviews
+  resources :books, :booklists, :reviews, :bookstatuses
   root 'books#index'
 
   # BOOKS
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_063233) do
+ActiveRecord::Schema.define(version: 2019_05_06_062806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2019_05_05_063233) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookstatuses", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "user_id"
+    t.integer "read_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_bookstatuses_on_book_id"
+    t.index ["user_id"], name: "index_bookstatuses_on_user_id"
   end
 
   create_table "rating_caches", force: :cascade do |t|
