179df4e9f5d6143cbbeaf74bd1841b9ff1c304dd
shiowwj <shiowweijun@gmail.com>
2019-05-07 15:54:49 +0800

    null

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,15 @@
 class Book < ActiveRecord::Base
+  include PgSearch
+
   has_and_belongs_to_many :booklists
   has_many :reviews
   has_many :bookstatuses
 
   self.per_page = 10
+
+  pg_search_scope :search_by_title, :against =>[:title],
+  using:{
+    :tsearch => {:prefix=>true}
+  }
+
 end
