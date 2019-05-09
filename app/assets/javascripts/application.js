ab3fe60c6b5460a35784cdab350bd4064fdebed5
valenlyn <valenlynchua@gmail.com>
2019-05-09 13:58:18 +0800

    Created dropdown list combining bookstatuses and booklists

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
