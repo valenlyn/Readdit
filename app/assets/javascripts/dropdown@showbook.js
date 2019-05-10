49cc8fd7246d7e38bb39b8c5ba5436b0177b5271
shiowwj <shiowweijun@gmail.com>
2019-05-10 17:11:39 +0800

    fix bugs

--- app/assets/javascripts/dropdown@showbook.js
+++ app/assets/javascripts/dropdown@showbook.js
@@ -1,16 +1,20 @@
 console.log('hi')
 
-
 // finds book and current user variable. To be passed into post request.
 var bookId = document.getElementsByClassName('book-id')[0].id;
 var userId = document.getElementsByClassName('current-user')[0].id;
+var currentStatus = document.getElementsByClassName('read-status-now');
+var onLoadString = document.getElementById('status-shelf');
 
 window.addEventListener('DOMContentLoaded', () =>{
     var dropDown = document.querySelectorAll("#drop-down-button");
-
+    console.log('the status now',currentStatus[0].id);
+    console.log('the thing to change: ', onLoadString.innerText);
+    initialDomButton(currentStatus[0].id);
     dropDown.forEach( item => {
         item.addEventListener('click', () => {
         updateStatus(event)
+        // highlightSelection(status);
         });
     })
 })
@@ -46,6 +50,8 @@ function changeDOMButton(jsonInput){
     // finds which button pressed. gets the innertext
     // statusShelfButton selects the collapsed button view
     // finally it changes the innertext of viewing button to what was selected by user
+
+
     var statusString = jsonInput.innerText;
     var statusShelfButton = document.getElementById('status-shelf');
 
@@ -65,3 +71,15 @@ function highlightSelection(status){
         }
     }
 }
+
+function initialDomButton(statusId){
+    if(statusId == 0 || statusId == 1){
+        onLoadString.innerText = 'Want to Read';
+    }else if(statusId == 2){
+        onLoadString.innerText = 'Currently Reading';
+    }else if(statusId ==3){
+        onLoadString.innerText = 'Read';
+    }
+}
+
+// initialDomButton(currentStatus[0].id);
