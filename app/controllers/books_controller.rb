6e134fb602d30c17b0dbbf6ad643473b553eabd2
shiowwj <shiowweijun@gmail.com>
2019-05-12 00:15:14 +0800

    fixed edit reviews bugs. Still working on booklist ajax.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,7 +71,6 @@ class BooksController < ApplicationController
   def show
     # find the book with the corresponding :id
     @book = Book.find(params[:id])
-
     #What about cases when this book don't have any reviews?
     #We will get an empty arr []
     arr = @book.reviews
@@ -89,6 +88,8 @@ class BooksController < ApplicationController
       # # # Pass this to our show view
       @avg_ratings = add_ratings / @book.reviews.length
     end
+
+    p @book
   end
 
 private
