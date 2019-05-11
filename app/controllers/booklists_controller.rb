224dba005eab056eeba8d15311709dfc92aee804
valenlyn <48078774+valenlyn@users.noreply.github.com>
2019-05-12 01:04:09 +0800

    Revert "Added ability to edit booklist names on /booklists, with JavaScript ~styling~"

--- app/controllers/booklists_controller.rb
+++ app/controllers/booklists_controller.rb
@@ -64,7 +64,7 @@ class BooklistsController < ApplicationController
     @booklist = Booklist.find(params[:id])
 
     @booklist.update(booklist_params)
-    redirect_to booklists_path
+    redirect_to @booklist
   end
 
   def destroy
@@ -103,7 +103,7 @@ class BooklistsController < ApplicationController
 
 private
 
-  def booklist_params
+  def book_params
     params.require(:booklist).permit(:user_id, :booklist_type)
   end
 end
